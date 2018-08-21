43214335e4cd0b3814f8d925486df501ff63ac3d
vladyslav.bukach <vladyslav.bukach@globallogic.com>
2018-08-21 18:02:15 +0300

    titles in separte file

--- script.js
+++ script.js
@@ -2,28 +2,21 @@
 const rank = document.getElementById('rank');
 const getTitleBtn = document.querySelector('.btn-2c');
 
-let titles = [ 
-    "card1",
-    'card2',
-    'card3',
-    'card4',
-    'card52',
-]
-
 
 
 // Listeners
 getTitleBtn.addEventListener('click', () => {
-    rank.textContent = getTitle();
+    rank.textContent = getTitle() + ', ' + getTitle() + ', ' + getTitle() ;
 }); 
     
 
 
 // Functions
-
 let getTitle = () => {
-    let title = Math.floor(Math.random() * titles.length);
-    return titles[title];
+    const titles = getRanks();
+    let randomIndex = Math.floor(Math.random() * titles.length);
+
+    return titles[randomIndex];
  };
 
 console.log(getTitle());
